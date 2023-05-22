bbd6a022cd3ef1222cbb8ac35cc8ccc4e1785492
yuta <eypc.ofun@gmail.com>
2023-05-23 01:38:24 +0900

    fix database.php

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -126,6 +126,10 @@ return [
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
+            'parameters' => [
+                'scheme' => env('REDIS_SCHEME', 'tcp'),
+                'password' => env('REDIS_PASSWORD', null),
+            ],
         ],
 
         'default' => [
@@ -145,6 +149,7 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
+            'scheme' => env('REDIS_SCHEME', 'tcp'),
         ],
 
     ],
